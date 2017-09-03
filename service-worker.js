424f1bd2484670a480c6aad5f1f8cd14c39ecc7b
caragarainfall <jryanmorz@gmail.com>
2017-09-03 12:47:00 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,9 @@ var cacheFiles = [
 	'https://cdnjs.cloudflare.com/ajax/libs/openlayers/2.13.1/OpenLayers.js',
 	'https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js',
 	'https://www.openstreetmap.org/',
-	'https://a.tile.openstreetmap.org/'
+	'//a.tile.openstreetmap.org/',
+        '//b.tile.openstreetmap.org/',
+        '//c.tile.openstreetmap.org/'
 ]
 
 
